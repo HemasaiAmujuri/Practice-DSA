8d2f1cb7da3323f3621866aaeb70d4675493dd46
Amujuri Hema Sai <hemasaiamujuri3@gmail.com>
2025-11-14 02:16:42 -0800

    solve palindrone string problem

--- DataStructures/Prerequisites/palindroneNumber.js
+++ DataStructures/Prerequisites/palindroneNumber.js
@@ -1,3 +1,6 @@
+                     //for numbers
+
+
 let num  = 14410;
 let rev = 0;
 let value = num;
@@ -14,4 +17,23 @@ if(value == rev){
     console.log(`${value} is a palindrone number`)
 }else {
     console.log(`${value} is not a palindrone number`)
+}
+
+
+                 //for strings
+let str = "madam"
+
+let revStr = ""
+ let n = str.length;
+
+for(i=n-1; i >= 0; i--){
+    revStr += str[i]
+}
+
+console.log(revStr)
+
+if(revStr === str ){
+    console.log(`${str} is a palindrone string`)
+}else{
+    console.log(`${str} is not a palindrone string`)
 }
