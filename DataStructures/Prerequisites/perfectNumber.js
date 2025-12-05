4e2d9d97d0a6326ac4a755e1be523227cd503cb3
Amujuri Hema Sai <hemasaiamujuri3@gmail.com>
2025-12-05 05:28:07 -0800

    solved perfect number

---
+++ DataStructures/Prerequisites/perfectNumber.js
@@ -0,0 +1,18 @@
+function perfectNumber(num){
+    let sum = 1;
+    for(i=2; i<num; i++){
+        if(num % i === 0){
+           sum += i;
+        }
+    }
+         console.log(sum,"sum");
+        if(sum === num){
+            console.log(sum,"sum");
+            return `${num} is a perfect number`
+        }
+        return num + " " +  'is not a perfect number'
+    }
+
+let num = 6
+const result = perfectNumber(num)
+console.log(result,"result")
