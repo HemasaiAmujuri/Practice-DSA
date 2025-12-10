235ebed5ada600251eab9c5a9fe962c4bc9f8ec0
Amujuri Hema Sai <hemasaiamujuri3@gmail.com>
2025-12-09 22:27:53 -0800

    solved strong number

---
+++ DataStructures/Prerequisites/strongNumber.js
@@ -0,0 +1,24 @@
+function strongNumber(num){
+
+    let original = num;
+    let sum = 0;
+    while(num > 0){
+        let temp = 1;
+        let  digit = num % 10;
+        for(i=1; i<=digit;i++){
+            temp *= i
+        }
+        sum+=temp
+        num = Math.floor(num/10)
+    }
+    if(original === sum){
+        return 'Its a Strong number'
+    }else{
+        return 'its not a Strong number'
+    }
+   
+}
+
+
+let num = 145;
+console.log(strongNumber(num))
