4bbb0b0aa85d17acaa03a16eb473b48c5a1ba879
Amujuri Hema Sai <hemasaiamujuri3@gmail.com>
2026-03-09 00:49:08 -0700

    solved prime number to reduce time complexity O(n) to O(root(n))

--- DataStructures/Prerequisites/primeNumber.js
+++ DataStructures/Prerequisites/primeNumber.js
@@ -19,19 +19,46 @@
 
                  //Method-2(efficient from method1)
 
-let num = 11;
-let count = 0;
+// let num = 11;
+// let count = 0;
 
 
-for(i =2; i< num; i++){
-    if(num % i === 0){
-        count++;
+// for(i =2; i< num; i++){
+//     if(num % i === 0){
+//         count++;
+//     }
+// }
+
+
+// if(count == 0){
+//     console.log(`${num} is a prime number`)
+// }else{
+//     console.log(`${num} is not a prime number`)
+// }
+
+
+                                       //Method-3(efficient from method2)
+
+
+function isPrime(num){
+  if( num <= 1){
+    return false
+  }
+
+  for(i = 2; i * i <= num; i++){
+    if (num % i === 0){
+        return false;
     }
+    return true;
+  }
 }
 
+const num = 34523;
+// const num = 34519;
 
-if(count == 0){
-    console.log(`${num} is a prime number`)
-}else{
-    console.log(`${num} is not a prime number`)
-}
+console.log(isPrime(num));
+
+//NOTE:Every factor larger than √n has a matching factor smaller than √n
+//EXPLAMNATION: “I check divisibility only up to the square root of the number because any factor larger than √n would have a corresponding smaller factor.
+// If no divisors are found up to √n, the number must be prime.
+// This reduces the time complexity from O(n) to O(√n), which is much more efficient for large numbers.
