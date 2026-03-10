c4e5b7c43fc34b1e3cf23ef7e6e1d7cbc7ae1450
Amujuri Hema Sai <hemasaiamujuri3@gmail.com>
2026-03-10 00:53:15 -0700

    solved optimised missing number program

--- DataStructures/Arrays/missingNumber.js
+++ DataStructures/Arrays/missingNumber.js
@@ -1,11 +1,24 @@
-function missingNumber(nums){
-   const length = nums.length;
-   for(let i = 0; i <= length; i++){
-    if(!nums.includes(i)){
-        return i;
-    }
+                      //Method-1
+// function missingNumber(nums){
+//    const length = nums.length;
+//    for(let i = 0; i <= length; i++){
+//     if(!nums.includes(i)){
+//         return i;
+//     }
+// }
+// }
+// const nums = [9,6,4,2,3,5,7,0,1];
+
+// console.log(missingNumber(nums));
+
+                       //Method-2
+function missingNumber(nums) {
+    const length = nums.length;
+    const expectedSum = (length * (length + 1)) / 2;
+    const actualSum = nums.reduce((acc, num) => acc + num, 0);
+    return expectedSum - actualSum;
 }
-}
+
 const nums = [9,6,4,2,3,5,7,0,1];
 
 console.log(missingNumber(nums));
