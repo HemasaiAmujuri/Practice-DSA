5e8a6b76b885137c68e94fdd810014f9ce59666c
HemasaiAmujuri <hemasaiamujuri3@gmail.com>
2026-05-21 13:39:02 +0530

    add conditions of leap year

--- DataStructures/Prerequisites/leapYear.js
+++ DataStructures/Prerequisites/leapYear.js
@@ -6,3 +6,11 @@ if( year % 4 == 0 && year % 100 != 0){
     console.log(`${year} is a Leap year`)
 }
 console.log(`${year} is not a leap year`)
+
+
+// A year is a leap year if:
+// 1. divisible by 4
+// AND
+// 2. not divisible by 100
+// OR
+// 3. divisible by 400
