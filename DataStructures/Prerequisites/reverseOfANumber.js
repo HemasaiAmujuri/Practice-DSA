c6ab1d4d3929f68ece8332781ae85be398ac2fd2
HemasaiAmujuri <hemasaiamujuri3@gmail.com>
2026-05-26 07:49:31 +0530

    solved reverse of a number in proper way

--- DataStructures/Prerequisites/reverseOfANumber.js
+++ DataStructures/Prerequisites/reverseOfANumber.js
@@ -1,10 +1,16 @@
-let num = 1234;
-let rev = 0;
+function reverse(x) {
+    let rev = 0;
+    let sign = x < 0 ? -1 : 1
 
-while(num > 0){
-    rem = num % 10
-    rev = rev*10 + rem
-    num = Math.floor(num / 10)
+    x = Math.abs(x);
+
+    while (x > 0) {
+        digit = x % 10
+        rev = rev * 10 + digit;  //is used to shift digits left before adding the new digit.
+        x = Math.floor(x / 10)
+    }
+    return rev * sign;
 }
 
-console.log(rev, "reverse");
+
+console.log(reverse(-123))
